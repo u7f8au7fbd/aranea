8d1d62cc6d76736440a4300ad66e1e75d91f8ff6
u7f8au7fbd <u7f8au7fbd@outlook.com>
2024-08-25 23:06:08 +0900

    set

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ impl Render for HelloWorld {
     fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
         div()
             .flex()
-            .bg(rgb(0x2e7d32))
+            .bg(rgba(0x2e7d3220)) // 背景色を透明にするために、ここの値を変更します
             .size(Length::Definite(Pixels(300.0).into()))
             .justify_center()
             .items_center()
             .shadow_lg()
             .border_1()
-            .border_color(rgb(0x0000ff))
+            .border_color(rgb(0x000000))
             .text_xl()
             .text_color(rgb(0xffffff))
             .child(format!("Hello, {}!", &self.text))
